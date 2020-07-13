940b7eafecc99c0a3d97dd44961a15838b75a75b
LeonidKursenko <leonid.kursenko.dev@gmail.com>
2020-07-13 18:26:06 +0300

    Add files via upload

---
+++ RestoreDB.sql
@@ -0,0 +1,6 @@
+﻿RESTORE DATABASE directorySettlementsDb
+FROM DISK = 'D:\Робота\Тестові завдання\Інфоплюс\initial'
+
+WITH MOVE 'directorySettlementsDb' TO 'C:\Users\Leonid\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\MSSQLLocalDB\directorySettlementsDb.mdf',
+MOVE 'directorySettlementsDb_log' TO 'C:\Users\Leonid\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\MSSQLLocalDB\directorySettlementsDb.ldf',
+REPLACE;
